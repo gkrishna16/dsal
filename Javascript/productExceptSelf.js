9d2b73b6c3cb01d62c88d44629f0578120bc8bc6
Gopal Krishan <gkrishna160@gmail.com>
2023-05-15 15:47:59 -0400

    added sol for java" ;

--- Javascript/productExceptSelf.js
+++ Javascript/productExceptSelf.js
@@ -1,16 +1,17 @@
 function productExceptSelf(nums) {
-  const answer = [];
-  let leftPrefix = 1;
-
-  for (let i = 0; i < nums.length; i++) {
-    answer[i] = leftPrefix;
-    leftPrefix *= nums[i];
+  let answer = [];
+  answer[0] = 1;
+  // answer array populated
+  for (let i = 1; i < nums.length; i++) {
+    answer[i] = nums[i - 1] * answer[i - 1];
   }
-
-  let rightPrefix = 1;
+  // console.log(answer);
+  // create right side of array and multiply right and left both to update the final array
+  let leftPrefix = 1;
   for (let j = nums.length - 1; j >= 0; j--) {
-    answer[j] *= rightPrefix;
-    rightPrefix *= nums[j];
+    answer[j] *= leftPrefix;
+    // console.log(answer);
+    leftPrefix *= nums[j];
   }
 
   return answer;
