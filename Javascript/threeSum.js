a10b64d85b8d879cf1b649b0286614d22e158f40
Gopal Krishan <gkrishna160@gmail.com>
2023-05-27 23:00:13 -0400

    added changes

--- Javascript/threeSum.js
+++ Javascript/threeSum.js
@@ -1,8 +1,44 @@
 function threeSum(nums) {
-  nums.sort();
+  nums.sort((a, b) => a - b);
+  let res = [];
 
-  return nums;
+  for (let i = 0; i < nums.length; i++) {
+    let first = nums[i];
+
+    if (first > 0) {
+      break;
+    }
+
+    if (i > 0 && first === nums[i - 1]) {
+      continue;
+    }
+
+    let l = i + 1,
+      r = nums.length - 1;
+
+    while (l < r) {
+      let currSum = first + nums[l] + nums[r];
+
+      if (currSum > 0) {
+        r--;
+      } else if (currSum < 0) {
+        l++;
+      } else {
+        res.push([first, nums[l], nums[r]]);
+        l++;
+        r--;
+        while (l < r && nums[l] === nums[l - 1]) {
+          l++;
+        }
+        while (l < r && nums[r] === nums[r + 1]) {
+          r--;
+        }
+      }
+    }
+  }
+
+  return res;
 }
 
-let val = threeSum([4, 3, 2, 1]);
+let val = threeSum([-1, 0, 1, 2, -1, -4]);
 console.log(val);
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,6 +1,6 @@
 function threeSum(nums: number[]): number[][] {
   nums.sort();
-  let res: number[][] | any = [];
+  let res: number[][] = [];
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > 0) {
       break;
@@ -8,8 +8,8 @@ function threeSum(nums: number[]): number[][] {
     if (i > 0 && nums[i] === nums[i - 1]) {
       continue;
     }
-    let l = i + 1;
-    let r = nums.length - 1;
+    let l: number = i + 1;
+    let r: number = nums.length - 1;
     while (l < r) {
       let threeSum = nums[i] + nums[l] + nums[r];
       if (threeSum > 0) {
@@ -19,6 +19,7 @@ function threeSum(nums: number[]): number[][] {
       } else {
         res.push([nums[i], nums[l], nums[r]]);
         l++;
+        r--;
         while (nums[l] === nums[l - 1] && l < r) {
           l++;
         }
